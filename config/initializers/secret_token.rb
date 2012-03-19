88df0a8ed92069ee2b4cadc3972179fb3dd69c61
Alex Berry <alexberry@Training-8.local>
2012-03-19 12:38:32 -0400

    added rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneTest::Application.config.secret_token = '3d53cfbf943096e94d09ff9ddcf0fdf8961e3bfa252cb09befc304890221997c5ce345d94a966877d12aa9bb514cc6dbbf5cf70d0924d83fccae7a532db637e9'
